4b7e1f5182d52b89641e8c10ead55ed0110e3c6e
FidelM345 <fidelomolo7@gmail.com>
2020-06-23 01:03:18 +0300

    menu add function

--- app/src/main/java/com/example/rentaladmin/MainActivity.java
+++ app/src/main/java/com/example/rentaladmin/MainActivity.java
@@ -22,36 +22,18 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorWhite));
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
 
-            }
-        });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.myproperty,
-                R.id.add_property)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+
+
     }
 
     @Override
@@ -61,27 +43,26 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
+
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.property_show:
+                Intent intent=new Intent(MainActivity.this,propertylist.class);
+                startActivity(intent);
+            case R.id.property_add:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
-//    public boolean onNavigationItemSelected(MenuItem item) {
-//        int id = item.getItemId();
-//        if (id == R.id.myproperty) {
-//            Intent intent=new Intent(MainActivity.this,propertylist.class);
-//            startActivity(intent);
-//
-//        }
-//        else if (id == R.id.add_property) {
-//
-//        }
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        drawer.closeDrawer(GravityCompat.START);
-//        return true;
-//    }
 
-       // navigationView.setNavigationItemSelectedListener
+
+
+
+
 }
